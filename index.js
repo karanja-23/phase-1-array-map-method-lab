1ff54086faf78ff294bc930335cbe408eb1a0718
Hosea <karanja.mungai23@gmail.com>
2024-09-30 21:49:57 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function (tutorial){
+    const words = tutorial.split(" ");
+    return words.map(function (word){
+     return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+  })
+ 
 }
